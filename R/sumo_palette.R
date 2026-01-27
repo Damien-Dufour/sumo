4d5a197ed97985cb6896337a36bd1b84fd2150ab
Damien-Dufour <97104712+Damien-Dufour@users.noreply.github.com>
2026-01-27 13:26:22 +0100

    Add diverging to SUMO_palette

--- R/sumo_palette.R
+++ R/sumo_palette.R
@@ -4,7 +4,9 @@
 #' @name sumo_palette
 #'
 #' @param x number of colour to display, by default 6
-#'
+#' @param diverging if TRUE, creates a purple to yellow diverging palette
+#' @param reverse to change direction
+#' #'
 #' @description create a palette of colours based on Hokusai wave
 #'
 #'
@@ -22,77 +24,93 @@
 #'
 #'
 #' @export
-sumo_pal <- function(x = 6)
+sumo_pal <- function(x = 6, diverging = F, reverse = F)
 {
   #create the basis of colours, this one is based on Hokusai's wave
 
-
+  if(diverging == F)
+  {
     pal <- c("#011640",
              "#2D5873",
              "#7BA696",
              "#BFBA9F",
              "#BF9663")
+  }
 
 
-    # convert hex to RGB
+  if(diverging == T)
+  {
+    pal <- c("#e39f31",
+             "white",
+             "#bc62ba")
 
-    rgb_col <- col2rgb(pal)
+  }
 
-    # we will soon need to go from RGB to col
+  if(reverse == T){
 
-    rgb2col <- function(rgbinput){
+    pal <- rev(pal)
 
-      convertrgb <- function(col){
-        rgb(rgbinput[1, col],
-            rgbinput[2, col],
-            rgbinput[3, col],
-            maxColorValue = 255)
-      }
-      # Apply the function
-      sapply(1:ncol(rgbinput), convertrgb)
+  }
+
+  # convert hex to RGB
+
+  rgb_col <- col2rgb(pal)
+
+  # we will soon need to go from RGB to col
+
+  rgb2col <- function(rgbinput){
+
+    convertrgb <- function(col){
+      rgb(rgbinput[1, col],
+          rgbinput[2, col],
+          rgbinput[3, col],
+          maxColorValue = 255)
     }
+    # Apply the function
+    sapply(1:ncol(rgbinput), convertrgb)
+  }
 
 
 
 
-    # Create intermediate colours as long as it has not reach x colours
+  # Create intermediate colours as long as it has not reach x colours
 
-    while(ncol(rgb_col) < x)
+  while(ncol(rgb_col) < x)
+  {
+
+    # generate odd number of column
+
+    odd <- seq(1,
+               by=2,
+               len=ncol(rgb_col))
+
+    # loop that create intermediate colours, since it create a new column between two, i only takes odd values
+
+    for(i in odd)
     {
-
-      # generate odd number of column
-
-      odd <- seq(1,
-                 by=2,
-                 len=ncol(rgb_col))
-
-      # loop that create intermediate colours, since it create a new column between two, i only takes odd values
-
-      for(i in odd)
+      if(i != ncol(rgb_col))
       {
-        if(i != ncol(rgb_col))
-        {
-          tmp <- (rgb_col[,i] + rgb_col[,i+1])/2
+        tmp <- (rgb_col[,i] + rgb_col[,i+1])/2
 
-          rgb_col <- cbind(rgb_col[,1:i],
-                           tmp,
-                           rgb_col[,(i+1):ncol(rgb_col)])
-        }
+        rgb_col <- cbind(rgb_col[,1:i],
+                         tmp,
+                         rgb_col[,(i+1):ncol(rgb_col)])
       }
-
     }
-    temp <- rgb2col(rgb_col)
+
+  }
+  temp <- rgb2col(rgb_col)
 
 
-    fn_cols <- colorRamp(temp,
-                         space = "Lab",
-                         interpolate = "spline")
+  fn_cols <- colorRamp(temp,
+                       space = "Lab",
+                       interpolate = "spline")
 
-    cols <- t(fn_cols(seq(0,
-                          1,
-                          length.out = x)))
+  cols <- t(fn_cols(seq(0,
+                        1,
+                        length.out = x)))
 
-    pal <- rgb2col(cols)
+  pal <- rgb2col(cols)
 
 
 
